fd286b5023ccfc95e377c0d41183f5522f31a69a
Rolf Ayrton Cesista <yopayton9@gmail.com>
2024-06-26 15:30:29 +0200

    ok

---
+++ Classe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    internal class Classe
+    {
+        public List<Eleve> eleves { get; private set; }
+        public List<string> matieres { get; private set; }
+        public string nomClasse {  get; private set; }
+        private readonly static int maxMatiere = 10;
+        private readonly static int maxEleve = 30;
+
+
+        public Classe(string nomClasse)
+        {
+            this.eleves = new List<Eleve>();
+            this.matieres = new List<string>();
+            this.nomClasse = nomClasse;
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (this.eleves.Count == maxEleve)
+            {
+                throw new Exception();
+            }
+            this.eleves.Add(new Eleve(prenom, nom));
+        }
+
+        public void ajouterMatiere(string matiere)
+        {
+            if (this.matieres.Count == maxEleve)
+            {
+                throw new Exception();
+            }
+            this.matieres.Add(matiere);
+        }
+
+        public double moyenneMatiere(int matiere) {
+            if(!this.eleves.Any()) throw new Exception();
+            double somme = 0;
+            foreach(Eleve eleve in this.eleves)
+            {
+                somme += eleve.moyenneMatiere(matiere);
+            }
+            return Math.Truncate(100 * somme/this.eleves.Count)/100;
+        }
+
+        public double moyenneGeneral()
+        {
+            if (!this.eleves.Any()) throw new Exception();
+            double somme = 0;
+            for (int matiere = 0; matiere < this.matieres.Count; matiere++)
+            {
+                somme += this.moyenneMatiere(matiere);
+            }
+            return Math.Truncate(100 * somme/this.matieres.Count)/100;
+        }
+
+    }
+}
+
---
+++ Eleve.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Eleve{
+        public string nom { get; private set; }
+        public string prenom { get; private set; }
+        private List<Note> note;
+        private static int maxNote = 200;
+        private Classe classe;
+        private Dictionary<int,double> sommeNotes;
+        private Dictionary<int, int> nbNotes;
+        public Eleve(string prenom, string nom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.note = new List<Note>();
+            this.sommeNotes = new Dictionary<int, double>();
+            this.nbNotes = new Dictionary<int, int>();
+
+        }
+
+        public void ajouterNote(Note note)
+        {
+            if (this.note.Count == maxNote)
+            {
+                throw new Exception("Nombre de note max deja atteint");
+            }
+            this.note.Add(note);
+
+            if (this.sommeNotes.ContainsKey(note.matiere))
+            {
+                this.sommeNotes[note.matiere]+= (double)note.note;
+                this.nbNotes[note.matiere]++;
+            }
+            else
+            {
+                this.sommeNotes.Add(note.matiere, (double)note.note);
+                this.nbNotes.Add(note.matiere, 1);
+            }
+        }
+
+        public double moyenneMatiere(int matiere)
+        {
+            if (!this.sommeNotes.ContainsKey(matiere)){
+                throw new Exception();
+            }else { 
+                return Math.Truncate( 100 * this.sommeNotes[matiere] / this.nbNotes[matiere])/100;
+            }
+        }
+
+        public double moyenneGeneral()
+        {
+            double somme = 0;
+            int nb = sommeNotes.Keys.Count;
+            foreach(int matiere in sommeNotes.Keys)
+            {
+                somme += moyenneMatiere(matiere);
+            }
+            return Math.Truncate(100 * (somme/nb)) / 100;
+        }
+    }
+}
